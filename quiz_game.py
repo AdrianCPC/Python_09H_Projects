a1dca81172685aae638f50ecbd0f67db51e3ae4f
AdrianPC <adriancamilopc@gmail.com>
2024-03-08 21:21:59 -0500

    firts steps quiz game

---
+++ quiz_game.py
@@ -0,0 +1,15 @@
+print('Bienvenido a mi juego de prueba!')
+
+playing = input('¿Quieres jugar? ')
+
+if playing != 'yes':
+    quit()
+
+print('Esta bien! Juguemos :D')
+
+#Questions
+answer = input('¿Qué significa CPU? ')
+if answer == 'unidad central de procesamiento':
+    print('Bien, hecho!')
+#Que significa CPU
+#Unidad central de procesamiento
