12f9c349fbbcfa373bf452123fba881a22edf59b
AdrianPC <adriancamilopc@gmail.com>
2024-03-13 21:28:53 -0500

    finish first game

--- quiz_game.py
+++ quiz_game.py
@@ -2,14 +2,41 @@ print('Bienvenido a mi juego de prueba!')
 
 playing = input('¿Quieres jugar? ')
 
-if playing != 'yes':
+if playing.lower() != 'yes':
     quit()
 
 print('Esta bien! Juguemos :D')
+score = 0
 
 #Questions
 answer = input('¿Qué significa CPU? ')
-if answer == 'unidad central de procesamiento':
+if answer.lower() == 'unidad central de procesamiento':
     print('Bien, hecho!')
+    score += 1
+else:
+    print('Incorrecto!')
 #Que significa CPU
 #Unidad central de procesamiento
+answer = input('¿Qué es una GPU?')
+if answer.lower() == 'unidad procesadora de graficos':
+    print('Bien, hecho')
+    score +=1
+else:
+    print('Incorrecto!')
+
+answer = input('¿Qué es la RAM?')
+if answer.lower() == 'memoria de acceso aleatorio':
+    print('Bien, hecho!')
+    score += 1
+else:
+    print('Incorrecto')
+
+answer = input('¿Qué es la PSU?')
+if answer.lower() == 'fuente de alimentación':
+    print('Bien,hecho!')
+    score +=1
+else:
+    print('Incorrecto')
+    
+print('Tienes ' + str(score) + ' de preguntas correctas!')
+print('Tienes ' + str((score/4)*100) + '%.')
