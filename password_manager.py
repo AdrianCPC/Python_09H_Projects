c24d42e24c0ff735a2356b1964995abeb7e756fb
AdrianPC <adriancamilopc@gmail.com>
2024-04-09 20:30:53 -0500

    new game created in py

---
+++ password_manager.py
@@ -0,0 +1,5 @@
+pwd = input("¿Cuál es la contraseña principal?")
+
+while True:
+    mode = input("¿Le gustaria adicionar una nueva contraseña o ver las existentes (ver,adicionar)?, click en Q para salir. ")
+
