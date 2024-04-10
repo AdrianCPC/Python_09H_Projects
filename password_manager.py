6361bdb78d5a37f3ea2ed277f3b67287ba414345
AdrianPC <adriancamilopc@gmail.com>
2024-04-09 20:48:05 -0500

    adding conditional file txt

--- password_manager.py
+++ password_manager.py
@@ -1,5 +1,26 @@
-pwd = input("¿Cuál es la contraseña principal?")
+main_pwd = input("¿Cuál es la contraseña principal?")
+
+def view():
+    pass
+
+def add():
+    name = input("Nombre de cuenta: ")
+    pwd = input("Contraseña: ")
+    
+    with open("passwords.txt", "a") as f:
+        f.write(name + "|" + pwd + "\n")
 
 while True:
     mode = input("¿Le gustaria adicionar una nueva contraseña o ver las existentes (ver,adicionar)?, click en Q para salir. ")
+    
+    if mode == "Q":
+        break
+    
+    if mode == "ver":
+        view()
+    elif mode == "adicionar":
+        add()
+    else:
+        print("Modo invalido. ")
+        continue
 
